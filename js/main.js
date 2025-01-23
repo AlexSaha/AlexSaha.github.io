7a1c00b751cf52000d2d23a1a9d5fc48669da08d
AlexSaha <alexsaha7@gmail.com>
2025-01-23 15:47:45 -0500

    updated wording

--- js/main.js
+++ js/main.js
@@ -93,7 +93,7 @@
 		$(window).scroll(function(){
 
 			var $win = $(window);
-			if ($win.scrollTop() > 200) {
+			if ($win.scrollTop() > 100) {
 				$('.js-top').addClass('active');
 			} else {
 				$('.js-top').removeClass('active');
@@ -102,32 +102,6 @@
 		});
 	
 	};
-
-	var pieChart = function() {
-		$('.chart').easyPieChart({
-			scaleColor: false,
-			lineWidth: 4,
-			lineCap: 'butt',
-			barColor: '#FF9000',
-			trackColor:	"#f5f5f5",
-			size: 160,
-			animate: 1000
-		});
-	};
-
-	var skillsWayPoint = function() {
-		if ($('#fh5co-skills').length > 0 ) {
-			$('#fh5co-skills').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( pieChart , 400);					
-					$(this.element).addClass('animated');
-				}
-			} , { offset: '90%' } );
-		}
-
-	};
-
 
 	// Loading page
 	var loaderPage = function() {
@@ -141,8 +115,6 @@
 		loaderPage();
 		fullHeight();
 		parallax();
-		// pieChart();
-		skillsWayPoint();
 	});
 
 
